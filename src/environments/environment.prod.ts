f7a04b30122f651169265a89b39d7ca5a1fb1447
StefanoVe <stefano.vecchietti.99@gmail.com>
2025-04-03 17:46:52 +0200

    deploy notify, modifica puntamento backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   localStorageJWT: 'legio.JWT',
-  apiUrl: 'https://backend.vps.legioneetruria.com',
+  apiUrl: 'https://legione-etruria-api.vps.notifyapp.it',
   debugAddItems: false,
   dimissTutorials: 'tutorials.dismiss',
 };
