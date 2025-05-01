43aa3f71035dcd8e97f11903aa4b4bac288288e4
StefanoVe <stefano.vecchietti.99@gmail.com>
2025-05-01 19:33:30 +0200

    fix widget width

--- src/app/legione-etruria/containers/home/home.component.ts
+++ src/app/legione-etruria/containers/home/home.component.ts
@@ -12,7 +12,7 @@ import { AuthService } from '../../../auth/services/auth.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  public CURRENT_VERSION = '0.88';
+  public CURRENT_VERSION = '0.89';
 
   public user$ = this.authService.currentUserSubject.pipe(
     tap((i) => (this.currentUser = i)),
